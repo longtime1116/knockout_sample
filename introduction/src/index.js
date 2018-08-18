33e2047ba5113aab1a4bdc14d128f47c0fe0cded
Tokinaga Shumo <longtime1116@gmail.com>
2018-08-18 14:53:46 +0900

    Making the data editable

--- introduction/src/index.js
+++ introduction/src/index.js
@@ -1,8 +1,9 @@
 const ko = require("knockout");
 // This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
 function AppViewModel() {
-  this.firstName = "Bert";
-  this.lastName = "Bertington";
+  // observable でくるむと、view -> viewModel の bind もするようになる
+  this.firstName = ko.observable("Bert");
+  this.lastName = ko.observable("Bertington");
 }
 
 // Activates knockout.js
