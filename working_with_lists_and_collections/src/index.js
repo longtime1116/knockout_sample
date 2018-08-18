716b2dd9cc386219fb20df8343ea665dfc9c6e31
Tokinaga Shumo <longtime1116@gmail.com>
2018-08-18 16:13:49 +0900

    Making the data editable

--- working_with_lists_and_collections/src/index.js
+++ working_with_lists_and_collections/src/index.js
@@ -5,6 +5,10 @@ function SeatReservation(name, initialMeal) {
   var self = this;
   self.name = name;
   self.meal = ko.observable(initialMeal);
+  self.formattedPrice = ko.computed(() => {
+    let price = self.meal().price;
+    return price ? price.toFixed(2) : "None";
+  });
 }
 
 // Overall viewmodel for this screen, along with initial state
